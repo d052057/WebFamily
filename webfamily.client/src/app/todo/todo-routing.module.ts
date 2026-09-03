85a276b3004d7bf8f74ef0ce0289a25530ebe0a6
Admin <Admin@YPHP>
2026-09-03 01:09:31 -0700

    move todomaint to admin

--- webfamily.client/src/app/todo/todo-routing.module.ts
+++ webfamily.client/src/app/todo/todo-routing.module.ts
@@ -3,23 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
-    loadComponent: () => import('./todo.component')
-      .then(mod => mod.TodoComponent),
-      children: [
-        {
-          path: 'todo-list',
-          loadComponent: () => import('./todo-list/todo-list.component')
-            .then(mod => mod.TodoListComponent)
-        },
-        {
-          path: 'todo-maint',
-          loadComponent: () => import('./todomaint/todomaint.component')
-            .then(mod => mod.TodoMaintComponent)
-        }
-      ]
-  },
-  {
     path: 'todo-list',
     loadComponent: () => import('./todo-list/todo-list.component')
       .then(mod => mod.TodoListComponent)
